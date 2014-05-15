8a07e1accd263cf72ae46e661e80d40b36fc8cb6
Tyler <tyler@ubuntu.ubuntu-domain>
2014-05-14 22:36:43 -0400

    foundations for exercise

--- module/HrphpTwitter/Module.php
+++ module/HrphpTwitter/Module.php
@@ -3,10 +3,16 @@
 namespace HrphpTwitter;
 
 use Zend\Mvc\MvcEvent;
+use HrphpTwitter\Twitter\TwitterClient;
 
 class Module
 {
 
+	public function getConfig()
+	{
+		return include __DIR__.'/config/module.config.php';
+	}
+	
     public function onBootstrap(MvcEvent $mvcEvent)
     {
     }
@@ -21,4 +27,24 @@ class Module
             ),
         );
     }
+    
+    public function getServiceConfig()
+    {
+    	return array(
+    		'invokables' => array(
+    				'HrphpTwitter\Twitter\TwitterFeedWrapper' => 'HrphpTwitter\Twitter\TwitterFeedWrapper',
+   			),
+    		'factories' => array(
+    			'HrphpTwitter\Twitter\TwitterClient' => function($sm) {
+    			$client = new TwitterClient();
+   				$settings = array('oauth_access_token' => $sm->get('config')['twitter']['oauth_access_token'],
+								   'oauth_access_token_secret' => $sm->get('config')['twitter']['oauth_access_token_secret'],
+								   'consumer_key' => $sm->get('config')['twitter']['consumer_key'],
+								   'consumer_secret' => $sm->get('config')['twitter']['consumer_secret']);
+   				$client->setSettings($settings);
+   				return $client;
+   			}
+  		));
+    }
 }
+
