56c13fd35bff8619f9b43e23c0b6078ebfa0edf6
kalmas <kylealmas@gmail.com>
2014-05-16 08:15:19 -0400

    print tweets to the screen

--- module/HrphpTwitter/config/module.config.php
+++ module/HrphpTwitter/config/module.config.php
@@ -2,9 +2,9 @@
 
 return [
 	'twitter' => [
-		'oauth_access_token' => "_",
-	    'oauth_access_token_secret' => "_",
-	    'consumer_key' => "_",
-	    'consumer_secret' => "_",
+		'oauth_access_token' => "96170534-TXdd18oyVmU4mDcrlmTasS55BMMedIf5LlYLeeF14",
+	    'oauth_access_token_secret' => "Be6AJvifrBxUZSrNodgxxmSuT4n8sNVn3RJxfvuBN6InP",
+	    'consumer_key' => "5PbbLeeWTEikdHF2LHirj44m7",
+	    'consumer_secret' => "yace7vmUCPyl8xSSZG4WXaIPellBQmBK6ZaDzklwXeg2F9AhYw",
 	],
 ];
--- module/HrphpTwitter/src/HrphpTwitter/Twitter/TwitterClient.php
+++ module/HrphpTwitter/src/HrphpTwitter/Twitter/TwitterClient.php
@@ -2,8 +2,8 @@
  
 namespace HrphpTwitter\Twitter;
 
-
-
+use TwitterAPIExchange;
+use Zend\Json\Json;
 class TwitterClient {
 	private $settings;
 	
@@ -15,6 +15,17 @@ class TwitterClient {
 	public function getUserFeed() {
 		//twitter api documentation https://dev.twitter.com/docs/api/1.1
 		//library documentation https://github.com/J7mbo/twitter-api-php
-		$twitter = new \TwitterAPIExchange($this->settings);						
+
+		$url = 'https://api.twitter.com/1.1/statuses/user_timeline.json';
+		$getfield = '?screen_name=hrphpmeetup';
+		$requestMethod = 'GET';
+
+		$twitter = new \TwitterAPIExchange($this->settings);
+		$tweets = $twitter->setGetfield($getfield)
+		             ->buildOauth($url, $requestMethod)
+		             ->performRequest();
+        $phpFeed = Json::decode($tweets, Json::TYPE_OBJECT);
+        return $phpFeed;
+								
 	}
 }           	
